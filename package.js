aa9ecc55a30b76406544503bb114c1a90de7ff5f
Patrick Lewis <Patrickml.pml@gmail.com>
2015-05-05 09:56:38 -0400

    Commenting Bones

--- package.js
+++ package.js
@@ -1,22 +1,52 @@
 Package.describe({
-  name: 'theplug:comments',
+  name: 'tpio:comments',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Commenting for any collection',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/tpio/comments',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
+both = ['client','server'];
+
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('comments.js');
+  //api.addFiles('comments.js');
+  api.use(
+    [
+      'templating'
+    ],
+    both);
+
+
+  api.export('Comments',['server','client']);
+
+  api.addFiles(
+    [
+      'lib/client/template.html',
+      'lib/client/template.js'
+    ],
+    'client');
+
+  api.addFiles(
+    [
+      'lib/both/comments.collection.js'
+    ],
+    both);
+
+  api.addFiles(
+    [
+      'lib/server/comments.publish.js'
+    ],
+    'server');
+
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('theplug:comments');
+  api.use('tpio:comments');
   api.addFiles('comments-tests.js');
 });
